30f259c9817b98bfdcb2b345ca7f9f2dc805cf49
Kenna1986 <le.buu.liem@gmail.com>
2014-07-31 14:17:14 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,22 +16,6 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'iwin_kenhtinhot');
-
-/** MySQL database username */
-define('DB_USER', 'iwin_kenhtinhot');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'Kenhtinhot@13');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
